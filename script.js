3e134adeb5e49ff263fb8bc35f6a2e1cfc5ae3cf
Vika <vikaten06@gmail.com>
2023-11-22 23:48:10 +0300

    Доделать адаптив кроме подвала

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// const gap = 8;
-
-// const carousel = document.getElementById("carousel"),
-//   content = document.getElementById("content"),
-//   next = document.getElementById("next"),
-//   prev = document.getElementById("prev");
-  
-// next.addEventListener("click", e => {
-//   carousel.scrollBy(width + gap, 0);
-//   if (carousel.scrollWidth !== 0) {
-//     prev.style.display = "flex";
-//   }
-// });
-// prev.addEventListener("click", e => {
-//   carousel.scrollBy(-(width + gap), 0);
-//   if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-//     next.style.display = "flex";
-//   }
-// });
-
-// let width = carousel.offsetWidth;
-// window.addEventListener("resize", e => (width = carousel.offsetWidth));
+document.querySelector('.burger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('open');
+})
